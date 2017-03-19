572a58623348194162c70158772b3e04123fc6c9
Aron Rotteveel <rotteveel.aron@gmail.com>
2017-03-19 20:30:17 +0100

    fixed variable naming conflict

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-if [ ! -e ~/.oh-my-zsh ]; then
+export ZSH=$HOME/.oh-my-zsh
+
+if [ ! -e $ZSH ]; then
         # Download and configure oh-my-zsh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
