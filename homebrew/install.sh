4963ab349621303831ba472c69fa2c1a886d9de3
Aron Rotteveel <rotteveel.aron@gmail.com>
2017-03-31 19:39:32 +0200

    cleaned up homebrew installer, added mas apps installer

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,12 +12,4 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Replace the system Git with a Homebrew version asap
-# @see http://rachelbythebay.com/w/2016/04/17/unprotected/
-brew install git
-sudo chmod -x /Applications/Xcode.app/Contents/Developer/usr/bin/git
-
-# Install homebrew packages
-brew install grc coreutils
-
 exit 0
