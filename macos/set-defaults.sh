cfe4da9310ac943e92c4d3b82055d27f8a7195da
Aron Rotteveel <rotteveel.aron@gmail.com>
2018-05-22 14:56:07 +0200

    dont create JPG screenshots. Caused conversion to TIFF when pasting in mail.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -56,7 +56,7 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 defaults write com.apple.screencapture location -string "${HOME}/Documents/Screenshots"
 
 # Save screenshots in JPG format
-defaults write com.apple.screencapture type -string "jpg"
+defaults write com.apple.screencapture type -string "png"
 
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
